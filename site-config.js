f83f5207aec396652ca38904e747116fed7617e5
StructureCollective <admin@structurecollective.com>
2026-08-13 22:52:38 -0400

    Update social links in site-config.js

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@
 window.STRUCTURE_COLLECTIVE_CONFIG = {
   email: "admin@structurecollective.com",
   location: "Greensboro, NC",
-  instagram: "",
-  linkedin: "",
+  instagram: "https://www.instagram.com/_structurecollective/",
+  linkedin: "https://www.linkedin.com/company/structurecollective/",
   consultationSubject: "Structure Collective Consultation Request"
 };
